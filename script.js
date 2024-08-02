070b15f77a4a24675694617a07ea9f7fbc2de06b
jumeireze07 <146956054+jumeireze07@users.noreply.github.com>
2024-08-02 09:26:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const caixaPrincipal =document.querySelector('caixa-principal');
+const caixaPrincipal =document.querySelector('caixa-pergunta');
+const caixaPrincipal =document.querySelector('caixa-alternativa');
+const caixaPrincipal =document.querySelector('caixa-resultado');
