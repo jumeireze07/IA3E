3b84853398e7e67931b776391721a3fe3b16e245
jumeireze07 <146956054+jumeireze07@users.noreply.github.com>
2024-08-08 07:46:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-const caixaPrincipal =document.querySelector('caixa-principal');
-const caixaPrincipal =document.querySelector('caixa-pergunta');
-const caixaPrincipal =document.querySelector('caixa-alternativa');
-const caixaPrincipal =document.querySelector('caixa-resultado');
+const caixaPrincipal=document.querySelector('caixa-principal');
+const caixaPergunta=document.querySelector('caixa-pergunta');
+const caixaResultado=document.querySelector('caixa-resultado');
+const caixaAlternativa=document.querySelector('caixa-alternativa');
+
+const pergunta=[
+    {
+        enunciado:"Pergunta 1",
+        Alternativa:["alternativa 1","Alternativa 2" ]
+    },
+
+    {
+        enunciado:"Pergunta 2",
+        Alternativa:["alternativa 1","Alternativa 2" ]
+    },
+    {
+        enunciado:"Pergunta 3",
+        Alternativa:["alternativa 1","Alternativa 2" ]  
+    }
+    ]       
+    let posicao= 0;
+    let perguntaAtual;
+
+    function mostrapergunta(){
+        
+    }
