83ddeeb734fc611890b4c311a038a9fe19343496
jumeireze07 <146956054+jumeireze07@users.noreply.github.com>
2024-09-13 12:08:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
-        enunciado:"Você gosta da Inteligencia Artificial",
+        enunciado:"Você gosta da Inteligencia Artificial?",
         alternativas:["Sim","Não"]
     },
     {
-        enunciado:"Você sabe ultilizar a internet de modo adequado",
+        enunciado:"Você sabe ultilizar a internet de modo adequado?",
         alternativas:["sim","Não",]
     },
     {
-        enunciado:"Você sabe mexer com as tecnilogia nova ",
+        enunciado:"Você sabe mexer com as tecnilogia nova? ",
         alternativas:["Sim","Não"]
     }
 ]
@@ -25,6 +25,10 @@ function mostraPergunta(){
     caixaPergunta.textContent = perguntaAtual.enunciado;  
     mostraAlternativas();
 }
-function mostraAlternativas();{
-    
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativa){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativa.appendChild(botaoAlternativas);
+    }
 }
