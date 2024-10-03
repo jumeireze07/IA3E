f3fefc6c5d0488543e2357120ff86037faf84978
jumeireze07 <146956054+jumeireze07@users.noreply.github.com>
2024-10-03 07:52:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,16 +6,41 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
         enunciado:"Você gosta da Inteligencia Artificial?",
-        alternativas:["Sim","Não"]
+        alternativas:[{
+            texto:"sim",
+            afirmaçâo:"sim,porque me ajudar a me informar  "
+        },
+       { texto:"Nâo",
+        afirmaçâo:"Nâo,pois e dificil de mexer"
+    }
+    ]
     },
     {
         enunciado:"Você sabe ultilizar a internet de modo adequado?",
-        alternativas:["sim","Não",]
+        alternativas:[{
+            texto:"sim",
+            afirmaçâo:"sim,pois sei ultilizar de modo essencial"
+        },
+      {  texto:"Nâo",
+        afirmaçâo:"Nâo,pois de modo denecessaro"}
+    ]
+    
     },
     {
         enunciado:"Você sabe mexer com as tecnilogia nova? ",
-        alternativas:["Sim","Não"]
-    }
+        alternativas:[{
+            texto:"sim",
+            afirmaçâo:"sim,pois faço cursinho de tecnologia"
+        },
+       { texto:"nao",
+        afirmaçâo:"nâo,pois nao sei o basico da tecnologia"}
+    ]
+    },
+    
+
+
+    
+
 ]
 let posicao = 0;
 let perguntaAtual;
